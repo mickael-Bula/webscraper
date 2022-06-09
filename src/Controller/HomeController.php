6570647553d1331d9e7441a81a2d52cf20f96d79
Locullus <bula.mickael@gmail.com>
2022-06-09 12:38:16 +0200

    exécution des requêtes en BDD depuis un service afin d'alléger le controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,44 +2,24 @@
 
 namespace App\Controller;
 
-use App\Repository\CacRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\DataScraper;
+use App\Service\SaveDataInDatabase;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function index(CacRepository $cacRepository): Response
+    public function index(SaveDataInDatabase $saveDataInDatabase): Response
     {
         // récupération des données par le scraper
         $data = DataScraper::getData();
-
-        // récupération de lastDate en BDD
-        $lastDate = $cacRepository->findLastDate();
-        if ( !empty($lastDate)) {
-            $lastDate = $lastDate[0]->getCreatedAt();
-            $lastDate = $lastDate->format('d/m/Y');
-        } else {
-            $lastDate = null;
-        }
-
-        // tri des entrées postérieures à lastDate
-        $newData = [];
-        foreach ($data as $row) {
-            if ( $lastDate !== $row[0]) {
-                $newData[] = $row;
-            }
-            else {
-                break;
-            }
-        }
-
-        // inversion du tableau pour que les nouvelles entrées soient ordonnées chronologiquement et insertion en BDD
-        $cacRepository->saveData(array_reverse($newData));
+        
+        // j'externalise l'insertion en BDD dans un service dédié
+        $lastDate = $saveDataInDatabase->appendData($data);
 
         // je crée une portion de tableau pour affichage
         $displayData = array_slice($data, 0, 10);
---
+++ src/Service/SaveDataInDatabase.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Cac;
+use Doctrine\ORM\EntityManagerInterface;
+
+class SaveDataInDatabase
+{
+    private $entityManager;
+
+    // pour accéder à Doctrine hors du controller, je dois injecter l'EntityManager
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * cette méthode insère dans la base les données postérieures à la dernière entrée disponible
+     * 
+     * @param array
+     * @return string|null
+     */
+    public function appendData($data): ?string
+    {
+        // je précise le Repository que je veux utiliser à mon EntityManager
+        $em = $this->entityManager;
+        $cacRepository = $em->getRepository(Cac::class);
+
+        // enfin, récupération de lastDate en BDD
+        $lastDate = $cacRepository->findLastDate();
+
+        if ( !empty($lastDate)) {
+            $lastDate = $lastDate[0]->getCreatedAt();
+            $lastDate = $lastDate->format('d/m/Y');
+        } else {
+            $lastDate = null;
+        }
+
+        // tri des entrées postérieures à lastDate
+        $newData = [];
+        foreach ($data as $row) {
+            if ( $lastDate !== $row[0]) {
+                $newData[] = $row;
+            }
+            else {
+                break;
+            }
+        }
+
+        // inversion du tableau pour que les nouvelles entrées soient ordonnées chronologiquement et insertion en BDD
+        $cacRepository->saveData(array_reverse($newData));
+
+        // on retourne la dernière date insérée en BDD pour information et affichage (on s'assure que le service a fonctionné)
+        return $lastDate;
+    }
+}
