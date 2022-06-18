fb4e7c8f5602cc4a4da1fa33f9738a74630c70df
Locullus <bula.mickael@gmail.com>
2022-06-18 16:18:23 +0200

    test de la classe DataScraper

---
+++ tests/Service/DataScraperTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\Tests\Service;
+
+use PHPUnit\Framework\TestCase;
+use App\Service\DataScraper;
+
+class DataScraperTest extends TestCase
+{
+    /**
+     * test vérifiant les données scrapées
+     */
+    public function testGetData()
+    {
+        // je crée un objet de la classe dataScraper pouir lancer le scraping
+        $dataScraper = new DataScraper();
+        $data = $dataScraper->getData();
+
+        // je fais une série de tests sur les données récupérées
+        $this->assertNotCount(0, $data);
+        $this->assertCount(23, $data);
+    }
+}
