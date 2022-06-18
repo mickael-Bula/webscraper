23b4bc9d774f98b300a4538ff8e9786ce9861186
Locullus <bula.mickael@gmail.com>
2022-06-18 21:44:58 +0200

    ajout de tests phpunit sur mes services

---
+++ tests/Service/UtilsTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Tests\Service;
+
+use PHPUnit\Framework\TestCase;
+use App\Service\Utils;
+use DateTime;
+
+class UtilsTest extends TestCase
+{
+    /**
+     * test de la transformation des données du cac en nombre décimal
+     * le format 1.234,56 est particulier, la règle US étant 1,234.56 et french 1 234,56
+     */
+    public function testFromString(): void
+    {
+        $utils = new Utils();
+        $this->assertEquals(6541.72, $utils->fromString("6.541,72"));
+    }
+
+    public function testGetMostRecentDate(): void
+    {
+        // je récupère le jour de veille ouvré calculé par mon Service
+        $utils = new Utils();
+        $date = $utils->getMostRecentDate();
+
+        // je crée un timestamp à partir de la date courante
+        $now = strtotime("now");
+
+        // j'en extrait le numéro du jour de la semaine ainsi que l'heure pour comparaison
+        $day = date("w", $now);
+        $hour = date("G", $now);
+
+        // s'il est moins de 18 h
+        if ($hour <= "18") {
+            // et que le jour n'est ni dimanche ni lundi
+            if (!in_array($day, ["0", "1"])) {
+                // on enlève un jour pour trouver le dernier jour ouvré
+                $x = 1;
+                // mais si on est dimanche
+            } else if ($day === "0") {
+                // on enlève 2 jours pour trouver le dernier jour ouvré
+                $x = 2;
+                // sinon on est lundi et ce sont 3 jours qu'il faut enlever
+            } else {
+                $x = 3;
+            }
+        // mais s'il est plus de 18 h
+        } else {
+            // si ce n'est pas le week-end
+            if (!in_array($day, ["0", "6"])) {
+                // le dernier jour ouvré est alors le jour en cours
+                $x = 0;
+            // mais si on est samedi
+            } else if ($day === "6") {
+                // on enlève un jour pour avoir le dernier jour ouvré
+                $x = 1;
+            // sinon on est alors dimanche
+            } else {
+                // et ce sont 2 jours que l'on doit ôter
+                $x = 2;
+            }
+        }
+        // je calcule un timestamp en soustrayant le résultat des conditions précédentes
+        $eve = strtotime("-{$x} day");
+        // je convertis le timestamp en chaîne
+        $eve = date("d/m/Y", $eve);
+        
+        // je teste enfin par comparaison cette chaîne avec celle recupérée du Service
+        $this->assertEquals($eve, $date);
+    }
+}
