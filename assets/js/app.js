257a22cefce262add286816e571e4de205952a81
Locullus <bula.mickael@gmail.com>
2022-06-24 09:57:23 +0200

    réduction de la taille des cards + changement de couleur du tableau + nouveau bg-color du body

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,14 +10,14 @@ const app = {
         const color = (card.id === "orange") ? "orange" : (card.id === "red") ? "red" : "green";
 
         if (card.classList.contains("active")) {
-            document.querySelector(`.my-card--${color} > .h4`).classList.remove("active");
+            document.querySelector(`.my-card--${color} > .h5`).classList.remove("active");
             document.querySelector(`.my-card--${color} > .my-card-positions`).classList.remove("active");
             card.classList.remove("active");
             // j'ajoute un timer pour faire disparaître la bordure une fois l'élément refermé
             setTimeout(() => card.classList.remove(`my-card--${color}`), 200);
         } else {
             card.className=`my-card my-card--${color} active`;
-            document.querySelector(`.my-card--${color} > .h4`).className="h4 active";
+            document.querySelector(`.my-card--${color} > .h5`).className="h5 active";
             document.querySelector(`.my-card--${color} > .my-card-positions`).className="my-card-positions active";
         }
     }
