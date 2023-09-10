f47d1405f4699e091b91e9f2753404409d2b02d4
Locullus <bula.mickael@gmail.com>
2023-09-10 16:42:06 +0200

    Actualise le modèle utilisé pour la récupération des données du site web après que sa présentation a changé

--- src/Service/DataScraper.php
+++ src/Service/DataScraper.php
@@ -13,13 +13,16 @@ class DataScraper
 
         // je filtre le document pour ne récupérer que le contenu du tableau qui m'intéresse
         $rawData = $crawler
-            ->filter('table[data-test="historical-data-table"] > tbody > tr > td')
+            ->filter('table > tbody > tr > td')
             ->each(function ($node) {
                 return $node->text('rien à afficher');
             });
         
         // la fonction array_chunk() divise le tableau passé en paramètre avec une taille fixée par le second
         $splitData = array_chunk($rawData, 7);
+
+        // on trie $splitData en vérifiant que le premier indice est une date au format jj/mm/aaaa
+        $splitData = array_filter($splitData, fn($row) => preg_match("/^\d{2}\/\d{2}\/\d{4}$/", $row[0]));
 
         // si l'on est un jour de semaine (lundi=1...) ET qu'il est moins de 18h, on considère le marché ouvert
         $isOpen = in_array(date('w'), range(1, 5)) && date("G") <= "18";
@@ -29,12 +32,7 @@ class DataScraper
             array_splice($splitData, 0, 1);
         }
 
-        // je boucle sur les résultats pour ne récupérer que les données utiles (date, closing, opening, higher, lower)
-        $data = [];
-        foreach($splitData as $chunk) {
-            $data[] = array_slice($chunk, 0, 5);
-        }
-        // on retourne le tableau
-        return $data;
+        // je filtre le tableau de résultats pour ne récupérer que les données utiles (date, closing, opening, higher, lower)
+        return array_map(fn($chunk) => array_slice($chunk, 0, 5), $splitData);
     }
 }
